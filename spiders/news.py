cccc3fbb35978b9fc3026b09415e6362cf9340a4
Akgit834 <Aadityakaushik834@gmail.com>
2024-03-24 23:38:52 +0530

    extracting data

--- spiders/news.py
+++ spiders/news.py
@@ -1,41 +1,40 @@
 import scrapy
 import re
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import json
 
 class NewsSpider(scrapy.Spider):
     name = "news"
     allowed_domains = ["timesofindia.indiatimes.com"]
     start_urls = ["https://timesofindia.indiatimes.com"]
 
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    
     def parse(self, response):
+        content={'data':[]}
         news=response.css("a.Hn2z7").getall()
         lis=[]
         for n in news:
             link=re.search(r'href="([^"]+\.cms)"',n)
             if link:
                 lis.append(link.group(1))
-        print("\n"*5,"content")
-        
         for l in lis:
-            yield response.follow(l,self.parse_page)
-               
-        print("\n"*5)
+            try:
+            #yield response.follow(l,self.parse_page)
+                r=requests.get(l,headers=self.headers)
+                soup=BeautifulSoup(r.text,'html.parser')             
+                text=soup.find('div', class_='_s30J clearfix')
+                if text:
+                    #print(text.get_text())
+                    content['data'].append(text.get_text())
+            except:
+                print("connection error")
+        #df=pd.DataFrame.from_dict(content)
+        #df.to_csv('test.csv')
 
-    def parse_page(self,response):
-        #scrapy.req
-        content=response.css('div[class^="_s30J"]').get()
-        
-        yield {'content':content}
-
-
-
-        # if div_text:
-        #     extracted_text = scrapy.Selector(text=div_text).xpath('//text()').getall()
-        #    
-        #     extracted_text = ''.join(extracted_text).strip()
-        #     yield{'text':extracted_text}
-        # else:
-        #     pass
-
-
+        with open('test.json','w') as f:
+            json.dump(content,f)
 
 
